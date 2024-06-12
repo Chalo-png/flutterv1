2e6d55ead555a02e7c05c9689c584d14aba140bc
Cristobal Carreno <cristobal.carrenof@usm.cl>
2024-06-11 23:28:38 -0400

    first commit; testing volume level detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,102 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_piano_audio_detection/flutter_piano_audio_detection.dart';
-import 'package:test2/rutas/generaMelodia.dart';
-import 'package:test2/rutas/practica.dart';
+import 'package:record/record.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MaterialApp(home: MicPage()));
 }
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-      routes: {
-        '/practica': (context) => PracticaScreen(),
-        '/generaMelodia': (context) => GeneraMelodiaScreen(),
-
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Piano App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButton(
-              text: 'Practica',
-              color: Colors.yellow,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PracticaScreen()),
-                );
-              },
-            ),
-            CustomButton(
-              text: 'Genera melodia',
-              color: Colors.red[200]!,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GeneraMelodiaScreen()),
-                );
-              },
-            ),
-            CustomButton(
-              text: 'Lecciones',
-              color: Colors.green[200]!,
-              onTap: () {
-                // Navigate to Lecciones Screen
-              },
-            ),
-            CustomButton(
-              text: 'Minijuegos',
-              color: Colors.blue[200]!,
-              onTap: () {
-                // Navigate to Minijuegos Screen
-              },
-            ),
-          ],
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );
   }
 }
 
-class CustomButton extends StatelessWidget {
-  final String text;
-  final Color color;
-  final VoidCallback onTap;
-
-  CustomButton({required this.text, required this.color, required this.onTap});
+class MicPage extends StatefulWidget {
+  const MicPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
+  State<MicPage> createState() => _MicPageState();
 }
+
+class _MicPageState extends State<MicPage> {
+  AudioRecorder myRecording = AudioRecorder();
+  Timer? timer;
+  
+  double volume = 0.0;
+  double minVolume = -45.0;
+    
+  startTimer() async{
+    timer ??= 
+      Timer.periodic(const Duration(milliseconds: 50), (timer) => updateVolume());
+  }
+
+  updateVolume() async {
+    Amplitude ampl = await myRecording.getAmplitude();
+    if(ampl.current > minVolume){
+      setState(() {
+        volume = (ampl.current - minVolume) / minVolume;
+      });
+  
+    }
+  }
+
+  int volume0to(int maxVolumeToDisplay){
+    return (volume * maxVolumeToDisplay).round().abs();
+  }
+
+  RecordConfig config = const RecordConfig(
+  // Set your desired configuration options here
+  // For example:
+  encoder: AudioEncoder.wav,
+);
+
+  Future<bool> startRecording() async {
+    if(await myRecording.hasPermission()){
+      if(!await myRecording.isRecording()){
+        Directory appDocDirectory = await getApplicationDocumentsDirectory();
+        String filePath = '${appDocDirectory.path}/record.wav';
+        await myRecording.start(path: filePath, config);
+        print("Recording started");
+      }
+      startTimer();
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  @override
+    Widget build(BuildContext context) {
+    final Future<bool> recordFutureBuilder =
+        Future<bool>.delayed(const Duration(seconds: 3), (() async {
+      return startRecording();
+    }));
+
+    return FutureBuilder(
+        future: recordFutureBuilder,
+        builder: (context, AsyncSnapshot<bool> snapshot) {
+          return Scaffold(
+            body: Center(
+                child: snapshot.hasData
+                    ? Text("VOLUME\n${volume0to(100)}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 42, fontWeight: FontWeight.bold))
+                    : const CircularProgressIndicator()),
+          );
+        });
+
+}}
