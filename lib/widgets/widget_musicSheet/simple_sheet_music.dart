e9a2575b93c814bf9e7265d7d12c3a71ca31dfda
jeremiasfeli <jeremiasfeli@gmail.com>
2024-06-19 21:46:51 -0400

    widget partitura addeed

---
+++ lib/widgets/widget_musicSheet/simple_sheet_music.dart
@@ -0,0 +1,24 @@
+/// The `simple_sheet_music` library provides a simple way to display sheet music in a Flutter application.
+/// It includes classes for rendering staffs, measures, clefs, notes, and other music objects.
+/// The `SimpleSheetMusic` widget is the main entry point for displaying sheet music.
+/// It takes a list of `Staff` objects and other optional parameters to customize the appearance of the sheet music.
+
+library simple_sheet_music;
+
+export 'src/measure/measure.dart' show Measure;
+export 'src/music_objects/clef/clef_type.dart';
+export 'src/music_objects/clef/clef.dart' show Clef;
+export 'src/music_objects/note/note.dart' show Note;
+export 'src/music_objects/interface/music_object_style.dart';
+export 'src/staff/staff.dart';
+export 'src/music_objects/note/parts/fingering.dart' show Fingering;
+export 'src/font_types.dart';
+export 'src/simple_sheet_music.dart'
+    show SimpleSheetMusic, OnTapMusicObjectCallback;
+export 'src/music_objects/note/note_duration.dart';
+export 'src/music_objects/note/note_pitch.dart';
+export 'src/music_objects/note/accidentals/accidental.dart' show Accidental;
+export 'src/music_objects/rest/rest.dart' show Rest;
+export 'src/music_objects/rest/rest_type.dart';
+export 'src/music_objects/key_signature/key_signature.dart'
+    show KeySignature, KeySignatureType;
---
+++ lib/widgets/widget_musicSheet/src/music_objects/clef/clef.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import '../../mixins/text_paint_mixin.dart';
+import '../interface/built_object.dart';
+import '../interface/music_object_on_canvas.dart';
+import '../interface/music_object_on_canvas_helper.dart';
+import '../interface/music_object_style.dart';
+import 'clef_type.dart';
+
+class Clef implements MusicObjectStyle, BuiltObject {
+  final ClefType clefType;
+  @override
+  final Color color;
+  @override
+  final EdgeInsets? specifiedMargin;
+  EdgeInsets get margin => specifiedMargin ?? _defaultMargin;
+
+  const Clef(this.clefType, {this.specifiedMargin, this.color = Colors.black});
+
+  EdgeInsets get _defaultMargin =>
+      EdgeInsets.symmetric(horizontal: clefType.width / 8);
+
+  @override
+  double get lowerHeight =>
+      bboxWithNoMargin.bottom + clefType.offsetHeight + margin.bottom;
+
+  @override
+  double get upperHeight =>
+      bboxWithNoMargin.top + clefType.offsetHeight + margin.top;
+
+  @override
+  double get width => clefType.width + margin.horizontal;
+
+  @override
+  BuiltObject build(ClefType clefType) => this;
+
+  Rect get bboxWithNoMargin => clefType.glyphBbox;
+
+  @override
+  ObjectOnCanvas placeOnCanvas(
+      {required double staffLineCenterY,
+      required double previousObjectsWidthSum}) {
+    final objectX = previousObjectsWidthSum + clefType.offsetX;
+    final objectY = staffLineCenterY + clefType.offsetHeight;
+    final renderOffset = Offset(objectX, objectY);
+    final helper = ObjectOnCanvasHelper(bboxWithNoMargin, renderOffset, margin);
+    return ClefPainter(clefType.glyph, helper, this);
+  }
+
+  @override
+  String toString() => 'Clef(ClefType: $clefType)';
+
+  @override
+  MusicObjectStyle copyWith(
+      {ClefType? newClefType,
+      Color? newColor,
+      EdgeInsets? newSpecifiedMargin}) {
+    return Clef(newClefType ?? clefType,
+        specifiedMargin: newSpecifiedMargin ?? specifiedMargin,
+        color: newColor ?? color);
+  }
+}
+
+class ClefPainter with TextPaint implements ObjectOnCanvas {
+  final String glyph;
+  @override
+  final ObjectOnCanvasHelper helper;
+  @override
+  final MusicObjectStyle musicObjectStyle;
+
+  const ClefPainter(this.glyph, this.helper, this.musicObjectStyle);
+
+  @override
+  void render(Canvas canvas, Size size, String fontFamily) {
+    textPaint(canvas, size, glyph, helper.renderOffset, musicObjectStyle.color,
+        fontFamily);
+  }
+
+  @override
+  bool isHit(Offset position) => helper.isHit(position);
+
+  @override
+  Rect get renderArea => helper.renderArea;
+}
